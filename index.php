0f0980d235207f1d23a59dafcf9c8452f4fbb309
Ethan Emerson <eemerson0022@pointloma.edu>
2024-12-08 09:51:52 -0800

    Basic Files added with document setup and population data

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- Website Game PHP Project: Ethan Emerson, Reese Bos, Jason Halabo, Ara Garabedian -->
+ <!DOCTYPE html>
+
+ <head>
+    <!-- metadata and Stylesheet Linking -->
+    <meta charset="UTF-8">
+    <link rel = "stylesheet" href="styles.css">
+    <title>Higher or Lower California Populations</title>
+ </head>
+
+ <!-- Create and start the body of our game-->
+  <body>
+    <h1>Higher or Lower!: California Populations!!!</h1>
+    <h2>By Ethan Emerson, Jason Halabo & Ara Garabedian</h2>
+
+  </body>
+ 
